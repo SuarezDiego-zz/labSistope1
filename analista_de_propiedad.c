976275113ec74bbb3f63a8d0da52447b46bed781
nicolascallahan <nicolas.callahan@usach.cl>
2018-05-18 20:18:39 -0400

    pixeles entre pipes arreglado

--- analista_de_propiedad.c
+++ analista_de_propiedad.c
@@ -4,46 +4,6 @@
 #include <unistd.h>
 #include "header_estructuras.h"
 
-/*
-Entrada: Un pixel RGB.
-Salida: Un pixel en escala de grises
-Descripcion: Funcion que convierte un pixel RGB a escala de grises
-*/
-Pixel pixel_a_negro(Pixel pix){
-	int ecucion_de_luminiscencia=pix.red*0.3+pix.green*0.59+pix.blue*0.11;
-	Pixel pixel_negro;
-	pixel_negro.red=ecucion_de_luminiscencia;
-	pixel_negro.green=ecucion_de_luminiscencia;
-	pixel_negro.blue=ecucion_de_luminiscencia;
-	pixel_negro.v=pix.v;
-	return pixel_negro;
-}
-
-/*
-Entrada: Arreglo con pixeles, cantidad de pixeles y un umbral.
-Salida: Entero. En caso de ser 1, la imagen es cercana a negro, en caso contrario, no lo es.
-Descripcion: Funcion que permite determinar si una imagen es cercana a negro.
-*/
-int nearlyBlack(Pixel* punteroPix, int cantidadPixeles, int umbral){
-	int i;
-	float porcentajeBlanco;
-	long contador_negros=0;
-	Pixel* punteroPixelesBinario=(Pixel*)malloc(cantidadPixeles*sizeof(Pixel));
-	for(i=0;i<cantidadPixeles;i++){
-		punteroPixelesBinario[i]=pixel_a_negro(punteroPix[i]);
-		porcentajeBlanco=(100*punteroPixelesBinario[i].red)/255;
-		if(porcentajeBlanco<=umbral){
-			contador_negros=contador_negros+1;
-		}
-	}
-	if((contador_negros*100)>((cantidadPixeles-contador_negros)*100)){
-		return 1;
-	}
-	else{
-		return 0;
-	}
-}
-
 
 /*
 Se debe tener acceso a pixeles, el cual sale del proceso lector_de_imagen y Estructura.
--- conversor_a_gris.c
+++ conversor_a_gris.c
@@ -4,164 +4,6 @@
 #include <unistd.h>
 #include "header_estructuras.h"
 
-/*
-Entrada: Un pixel RGB.
-Salida: Un pixel en escala de grises
-Descripcion: Funcion que convierte un pixel RGB a escala de grises
-*/
-Pixel pixel_a_negro(Pixel pix){
-	int ecucion_de_luminiscencia=pix.red*0.3+pix.green*0.59+pix.blue*0.11;
-	Pixel pixel_negro;
-	pixel_negro.red=ecucion_de_luminiscencia;
-	pixel_negro.green=ecucion_de_luminiscencia;
-	pixel_negro.blue=ecucion_de_luminiscencia;
-	pixel_negro.v=pix.v;
-	return pixel_negro;
-}
-
-/*
-Entrada: Arreglo con pixeles y la cantidad de pixeles
-Salida: Arreglo con todos los pixeles convertidos a escala de grises.
-Descripcion: Funcion que transforma todos los pixeles de un arreglo a una escala de grises.
-*/
-Pixel* pixeles_blanco_y_negro(Pixel* punteroPix, int cantidadPixeles){
-	int i;
-	Pixel* punteroPixelesBlancoYNegro=(Pixel*)malloc(cantidadPixeles*sizeof(Pixel));
-	for(i=0;i<cantidadPixeles;i++){
-		punteroPixelesBlancoYNegro[i]=pixel_a_negro(punteroPix[i]);
-	}
-	return punteroPixelesBlancoYNegro;
-}
-
-int stringAHexadecimal(char* stringEntrada){
-	int numeroHexa=0;
-	int i;
-	for(i=0;i<2;i++){
-		if(stringEntrada[i]==48){
-		}
-		else if(stringEntrada[i]==49){
-			if(i==0){
-				numeroHexa=numeroHexa+16;
-			}
-			else{
-				numeroHexa=numeroHexa+1;
-			}
-		}
-		else if(stringEntrada[i]==50){
-			if(i==0){
-				numeroHexa=numeroHexa+32;
-			}
-			else{
-				numeroHexa=numeroHexa+2;
-			}
-		}
-		else if(stringEntrada[i]==51){
-			if(i==0){
-				numeroHexa=numeroHexa+48;
-			}
-			else{
-				numeroHexa=numeroHexa+3;
-			}
-		}
-		else if(stringEntrada[i]==52){
-			if(i==0){
-				numeroHexa=numeroHexa+64;
-			}
-			else{
-				numeroHexa=numeroHexa+4;
-			}
-		}
-		else if(stringEntrada[i]==53){
-			if(i==0){
-				numeroHexa=numeroHexa+80;
-			}
-			else{
-				numeroHexa=numeroHexa+5;
-			}
-		}
-		else if(stringEntrada[i]==54){
-			if(i==0){
-				numeroHexa=numeroHexa+96;
-			}
-			else{
-				numeroHexa=numeroHexa+6;
-			}
-		}
-		else if(stringEntrada[i]==55){
-			if(i==0){
-				numeroHexa=numeroHexa+112;
-			}
-			else{
-				numeroHexa=numeroHexa+7;
-			}
-		}
-		else if(stringEntrada[i]==56){
-			if(i==0){
-				numeroHexa=numeroHexa+128;
-			}
-			else{
-				numeroHexa=numeroHexa+8;
-			}
-		}
-		else if(stringEntrada[i]==57){
-			if(i==0){
-				numeroHexa=numeroHexa+144;
-			}
-			else{
-				numeroHexa=numeroHexa+9;
-			}
-		}
-		else if(stringEntrada[i]==97){
-			if(i==0){
-				numeroHexa=numeroHexa+160;
-			}
-			else{
-				numeroHexa=numeroHexa+10;
-			}
-		}
-		else if(stringEntrada[i]==98){
-			if(i==0){
-				numeroHexa=numeroHexa+176;
-			}
-			else{
-				numeroHexa=numeroHexa+11;
-			}
-		}
-		else if(stringEntrada[i]==99){
-			if(i==0){
-				numeroHexa=numeroHexa+192;
-			}
-			else{
-				numeroHexa=numeroHexa+12;
-			}
-		}
-		else if(stringEntrada[i]==100){
-			if(i==0){
-				numeroHexa=numeroHexa+208;
-			}
-			else{
-				numeroHexa=numeroHexa+13;
-			}
-		}
-		else if(stringEntrada[i]==101){
-			if(i==0){
-				numeroHexa=numeroHexa+224;
-			}
-			else{
-				numeroHexa=numeroHexa+14;
-			}
-		}
-		else if(stringEntrada[i]==102){
-			if(i==0){
-				numeroHexa=numeroHexa+240;
-			}
-			else{
-				numeroHexa=numeroHexa+15;
-			}
-		}
-	}
-	return numeroHexa;
-}
 
 void escribirImagen(Pixel* punteroPixeles, unsigned char* header, char* nombreArchivo, int cantidadDePares){
 	int i;
@@ -195,6 +37,9 @@ argv[1] => nombreImagenSalidaEscalaG.
 void main(int argc, char *argv[]){
 	MensajePipe* mp=(MensajePipe*)malloc(sizeof(MensajePipe)+2000);
 	read(STDIN_FILENO, mp, sizeof(MensajePipe));
+	strcpy(mp->nombreImagen, "imagen_2.bmp");
+
+	Pixel* pixeles= obtenerPixelesPipe(mp);
 	printf("%i\n",mp->cantidadDePares );
 	//printf("%i\n", ppp[0].red);
 	/*
@@ -205,7 +50,7 @@ void main(int argc, char *argv[]){
 		printf("%i\n", mp->cabeza_imagen[j]);
 	}
 	
-	Pixel* pixelesbn=pixeles_blanco_y_negro(mp->pixeles,mp->cantidadDePares/4);
+	Pixel* pixelesbn=pixeles_blanco_y_negro(pixeles,mp->cantidadDePares/4);
 	escribirImagen(pixelesbn,mp->cabeza_imagen,"pico.bmp", mp->cantidadDePares);
 	free(pixelesbn);
 	printf("conversor_a_gris\n");
